f54fd6e8581e8a5b9abaf795f8d7678ffd953c4b
Chongchi Zhang <xiaochi1214@hotmail.com>
2015-12-26 00:23:04 -0600

    Web Components Practice
    Since I have an idea of a gallery, I am going to use web components to
    create a template for a each showing project, basically being similar to
    Pinterest Page. I want to practice using web components because I think
    it's important to get familiar with that technique. The final mostly
    focus on the featured project section

---
+++ data.js
@@ -0,0 +1,56 @@
+var projects = [{
+    name: '11323 S Eggleston Ave Chicago, IL 60628',
+    image: 'images/11323E400w.jpg',
+    title: '5 ROOMS &amp; ACCESSORY PACKAGE',
+    description: 'Kitchen: Table + 4 bar stools Living: Sofa + 2 end tables + coffee table/ottoman Dining: Table + 4 Chairs Family Room: Sofa + Coffee Table + end tables  Bedroom (Main level): Queen bed + dresser/chest + 1 night table/stand '
+}, {
+    name: '2757 N Lincoln Ave Chicago, IL 60614',
+    image: 'images/2757L400w.jpg',
+    title: '1 ROOM: $1000.00 FLAT RATE',
+    description: 'Living: Sofa + coffee table/ottoman'
+}, {
+    name: '7756 S Luella Ave Chicago, IL 60649',
+    image: 'images/7756L400w.jpg',
+    title: '5 ROOMS',
+    description: 'Living: 2- Sofas/Sectional + 1 decorative chair + 1-2 end tables + coffee table/ottoman Kitchen/Dining: Dining Table + 4 chairs Basement: Sofa &amp; Love Seat + coffee table Bedroom #1(1st floor): Queen Bed + Night stand + Chest'
+}, {
+    name: '7549 S Clyde Ave Chicago, IL 60649',
+    image: 'images/7549C400w.jpg',
+    title: '6 ROOMS',
+    description: 'Living: 2- Sofas/Sectional + 1 decorative chair + 1-2 end tables + coffee table/ottoman Kitchen/Dining: Table + 4-6 chairs Bedroom #1 (2nd floor): Queen Bed + 2 - Night stands + Dresser + Chest + Seating Bedroom #2 (1st floor): queen Bed + Night stand + Dresser Basement: 1-2 Sofas + Ottoman/Cocktail table + Sofa table'
+}, {
+    name: '1715 E 83rd St Chicago, IL 60617',
+    image: 'images/1715E83400w.jpg',
+    title: '5 ROOMS &amp; ACCESSORY PACKAGE',
+    description: 'Kitchen: Table + 4 chairs + 2-bar stools Living: Sofa + Loveseat/2 decorative chairs + 2 end tables + coffee table/ottoman Dining: Table + 4-6 Chairs/bench Family Room: Sofa + Coffee Table + 2- Accent Chairs + 3-Bar stools Master Bedroom: Queen bed + dresser + 2-night table/stand + 2- decorative chairs + side table'
+}, {
+    name: '4227 S Prairie Ave Chicago, IL 60653',
+    image: 'images/4227P400w.jpg',
+    title: '6 ROOMS:',
+    description: 'Living: 2- Sofas + 2 decorative chairs + 1-2 end tables + coffee table/ottoman Dining: Dining Table + 6-chairs or 4 –chairs/1 bench Basement: 2-Sofa bench + Game Table Master Bedroom: (2nd floor) King bed + dresser + 2-night table/stand + (1-2) decorative chairs/love seat + coffee table Guest Bedroom: (2nd floor) Queen bed + dresser/chest + (1-2) night table/stand Guest Bedroom: Day Ded Desk + Night stand + dresser'
+}, {
+    name: '4009 S Drexel Blvd Chicago, IL 60653',
+    image: 'images/4009D400w.jpg',
+    title: '6 ROOMS:',
+    description: 'Living: Sofa + Loveseat or 2 decorative chairs + 1-2 end tables + coffee table/ottoman Dining: Dining Table + 6-chairs or 4 –chairs/1 bench Basement: Sofa/Sectional or Sofa + 2-Decorative chairs + Ottoman/Coffee Table + End Table (optional) Master Bedroom: (2nd floor) Queen bed + dresser + (1-2) night table/stand + (1-2) decorative chairs + coffee table Guest Bedroom: (2nd floor) Queen bed + dresser/chest + (1-2) night table/stand Bedroom: (lower level) Desk + Office Chair + standing bookshelf'
+}, {
+    name: '3608 S Ellis Ave Chicago, IL 60653',
+    image: 'images/3608E400w.jpg',
+    title: '6 ROOMS &amp; ACCESSORY PACKAGE',
+    description: 'Living: Sofa + 2 end tables + coffee table/ottoman Dining: Table + 4 Chairs Family Room: Sofa + Coffee Table + end tables Bedroom #1: Queen bed + dresser/chest + 1 night table/stand Bedroom #2: Twin bed + dresser/chest + 1 night table/stand Bedroom #3: 1-2 decorative chair(s) + side table'
+}, {
+    name: '9729 S Princeton Ave Chicago, IL 60628',
+    image: 'images/9729P400w.jpg',
+    title: '4 ROOMS:',
+    description: 'Living: Sofa + Loveseat/2 chairs + 2 end tables + coffee/ottoman Family Room: Sectional/Sofa + Ottoman/Coffee Table Dining Room: Table + 6 chairs Master Bedroom: King bed + dresser + night table/stand + seating '
+}, {
+    name: '9520 S Damen Ave Chicago, IL 60643',
+    image: 'images/9520D400w.jpg',
+    title: '1 ROOM: $1000.00 FLAT RATE',
+    description: 'Living: Sofa + Loveseat/2 chairs + 2 end tables + coffee/ottoman Family Room: Sectional/Sofa + Ottoman/Coffee Table Dining Room: Table + 6 chairs Master Bedroom: King bed + dresser + night table/stand + seating '
+}, {
+    name: '8301 S Cregier Ave Chicago, IL 60617',
+    image: 'images/8301C400w.jpg',
+    title: '8 ROOMS:',
+    description: 'Living: Sofa + Loveseat/2 chairs + 2 end tables + coffee Family Room: Sectional + Ottoman/Coffee Table Kitchen: Table + 4 chairs Dining Room: Table + 6 chairs/4 chairs-2 benches Master Bedroom: Queen bed + dresser + night table/stand + seating Bedroom 1: Queen bed + Dresser + Nightstands Bedroom 2: Kid’s Twin Bed + Chest + Nightstand/table Bedroom 3: Twin beds + Chests + Nightstand/table '
+}];
